e5988b035ae09830af6f1e0074605f40862440d5
Felipe Buche <107374380+felipebuchee@users.noreply.github.com>
2023-11-26 23:13:25 -0300

    Create Ex03.php

---
+++ Ex03.php
@@ -0,0 +1,20 @@
+<?php //exercicio 3
+$sete_maravilhas = [
+    "Cristo Redentor",
+    "Chichén Itzá",
+    "Coliseu",
+    "Muralha da China",
+    "Petra",
+    "Machu Picchu",
+    "Taj Mahal"
+    ];
+    $valor_buscado = "Coliseu";
+    for($i=0; $i < 7; $i++){
+    if ($sete_maravilhas[$i] == $valor_buscado) {
+    print("o valor buscado está entre as maravilhas");
+    break;
+    }
+    }
+    
+// memória:
+// console: o valor buscado está entre as maravilhas
